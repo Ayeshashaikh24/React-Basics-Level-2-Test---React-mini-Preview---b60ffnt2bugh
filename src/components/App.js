277ce174a89f194dc729ac2099bc99ac3d62b5f2
Ayeshashaikh24 <97972259+Ayeshashaikh24@users.noreply.github.com>
2022-04-15 13:47:09 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,14 @@ const App = () => {
     <div id="main">
       <div>
         {/* add input elememnts inside here */}
+        <input type="text" id="contentInput" defaultValue='hello world' ></input>
+        <input type="number" id='fontSizeInput' defaultValue='10'></input>
+        <input type="number" id='paddingInput' defaultValue='5'></input>
       </div>
  
  {/* render Preview component here */}
  
+ <Preview ></Preview>
     </div>
   )
 }
